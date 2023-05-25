bfb89eb7cb27c5a8bad1facd3849aed718b6b789
Sojan SN <sojan.sn@frontiersin.net>
2023-05-25 13:12:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MongoDB_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
