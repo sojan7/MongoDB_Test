1b2b93ff5ee9d37a58f8f93859a89eeac0e29a83
Sojan SN <sojan.sn@frontiersin.net>
2023-05-25 14:45:17 +0530

    Main Updated

--- MongoDbMethods.cs
+++ MongoDbMethods.cs
@@ -1,13 +1,24 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 
 namespace MongoDB_Test
 {
     internal class MongoDbMethods
     {
-        public List<BsonDocument> GetListOfAllMongoDbsInADbClient(MongoClient dbClient)
+        public List<BsonDocument> GetListOfAllMongoDbsInADbClient(MongoClient dbClient) => dbClient.ListDatabases().ToList();
+
+        public BsonDocument GetTheBsonDocumentValueUsingTriggerId(MongoDatabase mongoDatabase, int triggerId)
         {
-            return dbClient.ListDatabases().ToList();
+            IMongoQuery query = Query.EQ("trigger.id", Convert.ToInt32(triggerId));
+            BsonDocument bsonDocument = mongoDatabase.GetCollection("report").FindOne(query);
+            return bsonDocument;
+        }
+
+        public MongoDatabase GetMongo(string connectionString, string databaseName)
+        {
+            MongoClient client = new(connectionString);
+            return client.GetServer().GetDatabase(databaseName);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,12 +9,15 @@ namespace MongoDB_Test
 
         public Program() => mongoDbMethods = new();
 
-        private MongoClient dbClient = new("mongodb://ss-stg-aira-quality:PLt0H9kVDtMTovX2@stgcluster01-shard-00-00.ddm82.azure.mongodb.net:27017,stgcluster01-shard-00-01.ddm82.azure.mongodb.net:27017,stgcluster01-shard-00-02.ddm82.azure.mongodb.net:27017/aira-quality?ssl=true&replicaSet=atlas-vo5aj6-shard-0&authSource=admin&retryWrites=true&w=majority");
+        private static readonly string? connectionString = "mongodb://ss-stg-aira-quality:PLt0H9kVDtMTovX2@stgcluster01-shard-00-00.ddm82.azure.mongodb.net:27017,stgcluster01-shard-00-01.ddm82.azure.mongodb.net:27017,stgcluster01-shard-00-02.ddm82.azure.mongodb.net:27017/aira-quality?ssl=true&replicaSet=atlas-vo5aj6-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+        private MongoClient dbClient = new(connectionString);
 
         private static void Main()
         {
             Program mainProgram = new();
-            List<BsonDocument> dataBases = mainProgram.mongoDbMethods.GetListOfAllMongoDbsInADbClient(mainProgram.dbClient);
+            MongoDatabase mongoDatabase = mainProgram.mongoDbMethods.GetMongo(connectionString!, "aira-quality");
+            BsonDocument element = mainProgram.mongoDbMethods.GetTheBsonDocumentValueUsingTriggerId(mongoDatabase, 31);
         }
     }
 }
