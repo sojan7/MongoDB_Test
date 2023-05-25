55189ba663cd7972a3faadb8b3c973ba93d9f39e
Sojan SN <sojan.sn@frontiersin.net>
2023-05-25 13:32:14 +0530

    Program Updated with MongoDB installation

---
+++ MongoDbMethods.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB_Test
+{
+    internal class MongoDbMethods
+    {
+        public List<BsonDocument> GetListOfAllMongoDbsInADbClient(MongoClient dbClient)
+        {
+            return dbClient.ListDatabases().ToList();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace MongoDB_Test
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB_Test
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private readonly MongoDbMethods mongoDbMethods;
+
+        public Program() => mongoDbMethods = new();
+
+        private MongoClient dbClient = new("mongodb://ss-stg-aira-quality:PLt0H9kVDtMTovX2@stgcluster01-shard-00-00.ddm82.azure.mongodb.net:27017,stgcluster01-shard-00-01.ddm82.azure.mongodb.net:27017,stgcluster01-shard-00-02.ddm82.azure.mongodb.net:27017/aira-quality?ssl=true&replicaSet=atlas-vo5aj6-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Program mainProgram = new();
+            List<BsonDocument> dataBases = mainProgram.mongoDbMethods.GetListOfAllMongoDbsInADbClient(mainProgram.dbClient);
         }
     }
 }
